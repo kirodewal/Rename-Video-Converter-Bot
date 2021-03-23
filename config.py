24c43384b42b5b8b38782c19798081bd0d877aba
Kamlesh Kirodewal <kkirodewal@gmail.com>
2021-03-23 07:20:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ class Config(object):
     # get a token from https://chatbase.com
     CHAT_BASE_TOKEN = "1bdaa716-3875-4d8c-be6b-c39a0e7f796e"
     # get a token from @BotFather
-    TG_BOT_TOKEN = "1449255969:AAGXQ1Ruj_6zuPNALkCOS6yvw85tUf4CPXw"
+    TG_BOT_TOKEN = "1449255969:AAGeayQ2k5RlIWzgMY6iXJ-dWQutoV70_Rs"
     # The Telegram API things
     APP_ID = "924859"
     API_HASH = "a4c9a18cf4d8cb24062ff6916597f832"
     # Get these values from my.telegram.org
     # Array to store users who are authorized to use the bot
-    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "").split())
+    AUTH_USERS = ""
     # Banned Unwanted Members..
     BANNED_USERS = []
     # the download location, where the HTTP Server runs
@@ -22,7 +22,7 @@ class Config(object):
     # chunk size that should be used with requests
     CHUNK_SIZE = "128"
     # default thumbnail to be used in the videos
-    DEF_THUMB_NAIL_VID_S = os.environ.get("DEF_THUMB_NAIL_VID_S", "https://placehold.it/90x90")
+    DEF_THUMB_NAIL_VID_S = ""
     # proxy for accessing youtube-dl in GeoRestricted Areas
     # Get your own proxy from https://github.com/rg3/youtube-dl/issues/1091#issuecomment-230163061
     HTTP_PROXY = os.environ.get("HTTP_PROXY", "")
@@ -33,7 +33,7 @@ class Config(object):
     # set timeout for subprocess
     PROCESS_MAX_TIMEOUT = 3600
     # watermark file
-    DEF_WATER_MARK_FILE = ""
+    DEF_WATER_MARK_FILE = "{file_name} Rename by @HxBots"
     # Database url
     DB_URI = "mongodb+srv://user1:user@cluster0.uwhes.mongodb.net"
     
